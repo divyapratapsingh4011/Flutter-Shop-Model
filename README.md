49d6d5ae3ed53db5427b34f9eaf394122802bb6b
divyapratapsingh4011 <divypratapsingh13@gmail.com>
2020-06-03 12:00:37 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Shop Model
+
+A practice Flutter project using firebase realtime database and Auth REST API.
---
+++ lib/links.dart
@@ -0,0 +1,5 @@
+class Links {
+  static const database = '';
+  static const authlink = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty';
+  static const apikey = '';
+}
