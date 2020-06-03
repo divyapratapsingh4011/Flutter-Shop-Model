83391801c4cf10f243098c1d97234a851b4fae29
divyapratapsingh4011 <divypratapsingh13@gmail.com>
2020-06-03 19:28:29 +0530

    change

---
+++ lib/links copy.dart
@@ -0,0 +1,6 @@
+class Links {
+  static const database = '';
+  static const authlink =
+      'https://www.googleapis.com/identitytoolkit/v3/relyingparty';
+  static const apikey = '';
+}
--- lib/links.dart
+++
@@ -1,5 +0,0 @@
-class Links {
-  static const database = '';
-  static const authlink = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty';
-  static const apikey = '';
-}
